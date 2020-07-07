3c09fde82d82b2db9ee57de4a78ee106efcd7315
gr <gremlin100004@yandex.ru>
2020-07-07 21:00:52 +0300

    [HW8] Test fabric beans with test class.

--- src/com/senla/carservice/Main.java
+++ src/com/senla/carservice/Main.java
@@ -1,10 +1,12 @@
 package com.senla.carservice;
 
-import com.senla.carservice.configuration.PackageScanner;
-import com.senla.carservice.util.PropertyLoader;
+import com.senla.carservice.configuration.Builder;
+import com.senla.carservice.testframework.MainService;
 
 public class Main {
     public static void main(String[] args) {
+        MainService mainService = (MainService) Builder.getInstance().createObject(MainService.class);
+        mainService.run();
 
 //        MenuController menuController = MenuController.getInstance();
 //        menuController.run();
---
+++ src/com/senla/carservice/testframework/Repository.java
@@ -0,0 +1,5 @@
+package com.senla.carservice.testframework;
+
+public interface Repository {
+    String getValue();
+}
