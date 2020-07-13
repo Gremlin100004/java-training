bd7856200412a395a46db2113ed3b55722076b24
gr <gremlin100004@yandex.ru>
2020-07-13 23:51:43 +0300

    [HW8] Start refactor project

---
+++ src/com/senla/carservice/factory/Context.java
@@ -0,0 +1,31 @@
+package com.senla.carservice.factory;
+
+import com.senla.carservice.factory.annotation.Prototype;
+import com.senla.carservice.factory.configurator.Configurator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Context {
+    private Builder builder;
+    private final Map<Class<?>, Object> cacheSingleton = new HashMap<>();
+    private Configurator configurator;
+
+    private Context(final Configurator configurator) {
+        this.configurator = configurator;
+    }
+
+    public <T> void addSingleton(Class<? extends T> implementClass, T singletonObject) {
+        if (implementClass.isAnnotationPresent(Prototype.class)) {
+            return;
+        }
+        cacheSingleton.put(implementClass, singletonObject);
+    }
+
+    public <T> T getObjectSingleton(Class<? extends T> implementClass) {
+        if (cacheSingleton.containsKey(implementClass)) {
+            return (T) cacheSingleton.get(implementClass);
+        }
+        return null;
+    }
+}
