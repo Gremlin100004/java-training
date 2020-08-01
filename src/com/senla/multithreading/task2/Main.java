cedbde6fbf9999feb69507651152d3e9590f7bf9
gr <gremlin100004@yandex.ru>
2020-08-01 18:54:47 +0300

    [HW9] Create thread with lambda in task 2

--- src/com/senla/multithreading/task2/Main.java
+++ src/com/senla/multithreading/task2/Main.java
@@ -1,16 +1,33 @@
 package com.senla.multithreading.task2;
 
 import com.senla.multithreading.task2.repository.FlagRepository;
-import com.senla.multithreading.task2.runnable.RunnableImpl;
 
 public class Main {
     public static void main(String[] args) {
         int numberOfCycles = 10;
         FlagRepository resource = new FlagRepository();
-        Thread firstThread = new Thread(new RunnableImpl(resource, numberOfCycles, true));
+        Thread firstThread = new Thread(() -> {
+            int i = 0;
+            while (i < numberOfCycles) {
+                if (resource.getFlag()) {
+                    System.out.println(Thread.currentThread().getName());
+                    resource.changeFlag();
+                    i++;
+                }
+            }
+        });
         firstThread.setName("First thread");
         firstThread.start();
-        Thread secondThread = new Thread(new RunnableImpl(resource, numberOfCycles, false));
+        Thread secondThread = new Thread(() -> {
+            int i = 0;
+            while (i < numberOfCycles) {
+                if (!resource.getFlag()) {
+                    System.out.println(Thread.currentThread().getName());
+                    resource.changeFlag();
+                    i++;
+                }
+            }
+        });
         secondThread.setName("Second thread");
         secondThread.start();
     }
