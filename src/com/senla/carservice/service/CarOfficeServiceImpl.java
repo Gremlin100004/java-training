723aae9f809c234444bc7bd4d5a7e2312538e7c9
gr <gremlin100004@yandex.ru>
2020-08-22 17:24:27 +0300

    [HW11] Add transaction to CarOfficeServiceImpl

--- src/com/senla/carservice/service/CarOfficeServiceImpl.java
+++ src/com/senla/carservice/service/CarOfficeServiceImpl.java
@@ -5,6 +5,7 @@ import com.senla.carservice.container.objectadjuster.dependencyinjection.annotat
 import com.senla.carservice.dao.MasterDao;
 import com.senla.carservice.dao.OrderDao;
 import com.senla.carservice.dao.PlaceDao;
+import com.senla.carservice.dao.connection.DatabaseConnection;
 import com.senla.carservice.domain.Master;
 import com.senla.carservice.domain.Order;
 import com.senla.carservice.domain.Place;
@@ -31,6 +32,8 @@ public class CarOfficeServiceImpl implements CarOfficeService {
     private CsvOrder csvOrder;
     @Dependency
     private CsvMaster csvMaster;
+    @Dependency
+    private DatabaseConnection databaseConnection;
     private static final int NUMBER_DAY = 1;
 
     public CarOfficeServiceImpl() {
@@ -38,73 +41,128 @@ public class CarOfficeServiceImpl implements CarOfficeService {
 
     @Override
     public Date getNearestFreeDate() {
-        if (masterDao.getAllRecords().isEmpty()) {
-            throw new BusinessException("There are no masters");
-        }
-        if (orderDao.getAllRecords().isEmpty()) {
-            throw new BusinessException("There are no orders");
-        }
-        if (placeDao.getAllRecords().isEmpty()) {
-            throw new BusinessException("There are no places");
-        }
-        Date leadTimeOrder = orderDao.getLastOrder().getLeadTime();
-        Date dayDate = new Date();
-        for (Date currentDay = new Date(); leadTimeOrder.before(currentDay);
-             currentDay = DateUtil.addDays(currentDay, NUMBER_DAY)) {
-            if (masterDao.getFreeMasters(currentDay).isEmpty() ||
-                placeDao.getFreePlaces(currentDay).isEmpty()) {
-                dayDate = currentDay;
-                currentDay = DateUtil.bringStartOfDayDate(currentDay);
-            } else {
-                break;
+        try {
+            databaseConnection.disableAutoCommit();
+            if (masterDao.getAllRecords().isEmpty()) {
+                throw new BusinessException("There are no masters");
             }
+            if (orderDao.getAllRecords().isEmpty()) {
+                throw new BusinessException("There are no orders");
+            }
+            if (placeDao.getAllRecords().isEmpty()) {
+                throw new BusinessException("There are no places");
+            }
+            Date leadTimeOrder = orderDao.getLastOrder().getLeadTime();
+            Date dayDate = new Date();
+            for (Date currentDay = new Date(); leadTimeOrder.before(currentDay);
+                 currentDay = DateUtil.addDays(currentDay, NUMBER_DAY)) {
+                if (masterDao.getFreeMasters(currentDay).isEmpty() ||
+                    placeDao.getFreePlaces(currentDay).isEmpty()) {
+                    dayDate = currentDay;
+                    currentDay = DateUtil.bringStartOfDayDate(currentDay);
+                } else {
+                    break;
+                }
+            }
+            databaseConnection.commitTransaction();
+            return dayDate;
+        } catch (BusinessException e) {
+            databaseConnection.rollBackTransaction();
+            throw new BusinessException("Error transaction get masters");
+        } finally {
+            databaseConnection.enableAutoCommit();
         }
-        return dayDate;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public void importEntities() {
-        masterDao.updateAllRecords(csvMaster.importMasters());
-        placeDao.updateAllRecords(csvPlace.importPlaces());
-        List<Order> orders = csvOrder.importOrder(masterDao.getAllRecords(), placeDao.getAllRecords());
-        orderDao.updateAllRecords(orders);
+        try {
+            databaseConnection.disableAutoCommit();
+            masterDao.updateAllRecords(csvMaster.importMasters());
+            placeDao.updateAllRecords(csvPlace.importPlaces());
+            List<Order> orders = csvOrder.importOrder(masterDao.getAllRecords(), placeDao.getAllRecords());
+            orderDao.updateAllRecords(orders);
+            databaseConnection.commitTransaction();
+        } catch (BusinessException e) {
+            databaseConnection.rollBackTransaction();
+            throw new BusinessException("Error transaction get masters");
+        } finally {
+            databaseConnection.enableAutoCommit();
+        }
+
     }
 
     @Override
     public void exportEntities() {
-        List<Order> orders = getOrders();
-        List<Master> masters = getMasters();
-        List<Place> places = getPlaces();
-        csvOrder.exportOrder(orders);
-        csvMaster.exportMasters(masters);
-        csvPlace.exportPlaces(places);
+        try {
+            databaseConnection.disableAutoCommit();
+            List<Order> orders = getOrders();
+            List<Master> masters = getMasters();
+            List<Place> places = getPlaces();
+            csvOrder.exportOrder(orders);
+            csvMaster.exportMasters(masters);
+            csvPlace.exportPlaces(places);
+            databaseConnection.commitTransaction();
+        } catch (BusinessException e) {
+            databaseConnection.rollBackTransaction();
+            throw new BusinessException("Error transaction get masters");
+        } finally {
+            databaseConnection.enableAutoCommit();
+        }
     }
 
     @SuppressWarnings("unchecked")
     private List<Order> getOrders() {
-        List<Order> orders = orderDao.getAllRecords();
-        if (orders.isEmpty()) {
-            throw new BusinessException("There are no orders");
+        try {
+            databaseConnection.disableAutoCommit();
+            List<Order> orders = orderDao.getAllRecords();
+            if (orders.isEmpty()) {
+                throw new BusinessException("There are no orders");
+            }
+            databaseConnection.commitTransaction();
+            return orders;
+        } catch (BusinessException e) {
+            databaseConnection.rollBackTransaction();
+            throw new BusinessException("Error transaction get masters");
+        } finally {
+            databaseConnection.enableAutoCommit();
         }
-        return orders;
     }
 
     @SuppressWarnings("unchecked")
     private List<Master> getMasters() {
-        List<Master> masters = masterDao.getAllRecords();
-        if (masters.isEmpty()) {
-            throw new BusinessException("There are no masters");
+        try {
+            databaseConnection.disableAutoCommit();
+            List<Master> masters = masterDao.getAllRecords();
+            if (masters.isEmpty()) {
+                throw new BusinessException("There are no masters");
+            }
+            databaseConnection.commitTransaction();
+            return masters;
+        } catch (BusinessException e) {
+            databaseConnection.rollBackTransaction();
+            throw new BusinessException("Error transaction get masters");
+        } finally {
+            databaseConnection.enableAutoCommit();
         }
-        return masters;
     }
 
     @SuppressWarnings("unchecked")
     private List<Place> getPlaces() {
-        List<Place> places = placeDao.getAllRecords();
-        if (places.isEmpty()) {
-            throw new BusinessException("There are no places");
+        try {
+            databaseConnection.disableAutoCommit();
+            List<Place> places = placeDao.getAllRecords();
+            if (places.isEmpty()) {
+                throw new BusinessException("There are no places");
+            }
+            databaseConnection.commitTransaction();
+            return places;
+        } catch (BusinessException e) {
+            databaseConnection.rollBackTransaction();
+            throw new BusinessException("Error transaction get masters");
+        } finally {
+            databaseConnection.enableAutoCommit();
         }
-        return places;
     }
 }
