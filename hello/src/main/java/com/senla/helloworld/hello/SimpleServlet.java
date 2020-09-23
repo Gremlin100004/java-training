b3c8e6d1dd1862f139e2ef167c7919a1e92bf0eb
gr <gremlin100004@yandex.ru>
2020-09-24 00:43:54 +0300

    [HW16] Add maven-plugin to project

--- hello/src/main/java/com/senla/helloworld/hello/SimpleServlet.java
+++ hello/src/main/java/com/senla/helloworld/hello/SimpleServlet.java
@@ -1,6 +1,5 @@
-package com.senla.servletbasedwebapp.servlet;
+package com.senla.helloworld.hello;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,14 +7,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = {"/hello"})
+@WebServlet(urlPatterns = {"/"})
 public class SimpleServlet extends HttpServlet {
 
     @Override
-    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
-        throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
         PrintWriter printWriter = resp.getWriter();
-        printWriter.println("HelloWorld");
+        printWriter.println("Hello world");
+        printWriter.close();
     }
 }
