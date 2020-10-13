c3ca3e54a5571f60d02f9eae6019e788b9ef7cec
gr <gremlin100004@yandex.ru>
2020-10-13 21:14:30 +0300

    [HW17] overriding methods of ResponseEntityExceptionHandler

--- controller/src/main/java/com/senla/carservice/controller/config/WebConfig.java
+++ controller/src/main/java/com/senla/carservice/controller/config/WebConfig.java
@@ -3,11 +3,10 @@ package com.senla.carservice.controller.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @ComponentScan("com.senla.carservice")
 @PropertySource("classpath:application.properties")
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
 }
