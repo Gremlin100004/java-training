8d1654578a748b6a479fcabf94cb66bc64f862c8
gr <gremlin100004@yandex.ru>
2020-11-30 02:17:31 +0300

    [Final] Correct checkstyle mistakes

--- service/src/main/java/com/senla/socialnetwork/service/PublicMessageServiceImpl.java
+++ service/src/main/java/com/senla/socialnetwork/service/PublicMessageServiceImpl.java
@@ -22,7 +22,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 import java.util.List;
 
 @Service
